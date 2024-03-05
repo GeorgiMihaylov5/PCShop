ba25fbfd9b7ac28b871bd0724e681fb417db504d
AntonMetodiev <toni2riemail@abv.bg>
2024-03-05 22:05:03 +0200

    Database Initial Creation

---
+++ PCShop/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PCShop.Entities;
+
+namespace PCShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderedProduct> OrderedProducts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ PCShop/Entities/OrderedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PCShop.Entities
+{
+    public class OrderedProduct
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [MaxLength(36)]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(36)]
+        public string ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [MaxLength(36)]
+        public string OrderId { get; set; }
+        public virtual Order Order { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Count { get; set; }
+        public bool DiscountApplied { get; set; }
+        
+    }
+}
