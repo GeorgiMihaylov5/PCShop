510d617a75ef5dd4481c51121e84da3a075deab4
Георги Михайлов <gogogm59@gmail.com>
2024-03-11 21:37:52 +0200

    Add base users views

---
+++ PCShop/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PCShop.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ PCShop/Models/UserVM.cs
@@ -0,0 +1,41 @@
+﻿using PCShop.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PCShop.Models
+{
+    public class UserVM
+    {
+        [MaxLength(36)]
+        public string Id { get; set; }
+
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [MaxLength(64)]
+        [Required]
+        public string Username { get; set; }
+
+        [MaxLength(64)]
+        [Required]
+        public string Email { get; set; }
+
+        //This will be hashed value, so no max lenght
+        [Required]
+        public string Password { get; set; }
+
+        [Range(14, 120)]
+        public int Age { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+
+        public bool IsAdministrator { get; set; }
+    }
+}
