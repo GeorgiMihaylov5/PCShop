4e0f1a75574b3fe7ad370f45f44662aed84c25b0
Георги Михайлов <gogogm59@gmail.com>
2024-03-11 22:19:26 +0200

    Add userService

--- PCShop/Data/Migrations/20240311194130_Add-User-Role.Designer.cs
+++ PCShop/Data/Migrations/20240311194130_Add-User-Role.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PCShop.Data;
 
@@ -11,9 +12,10 @@ using PCShop.Data;
 namespace PCShop.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240311194130_Add-User-Role")]
+    partial class AddUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
