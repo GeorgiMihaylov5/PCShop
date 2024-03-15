0baf5fcc63e668f19d6e38cba66bcf66170885ce
Георги Михайлов <gogogm59@gmail.com>
2024-03-15 02:39:04 +0200

    Add new view model

--- PCShop/Models/RegisterVM.cs
+++ PCShop/Models/RegisterVM.cs
@@ -2,16 +2,8 @@
 
 namespace PCShop.Models
 {
-    public class RegisterVM
+    public class RegisterVM : CreateEmployeeVM
     {
-        [MaxLength(30)]
-        public string FirstName { get; set; }
-        [MaxLength(30)]
-        public string LastName { get; set; }
-        [Required]
-        public string Username { get; set; }
-        [Required]
-        public string Email { get; set; }
         [Required]
         public string Password { get; set; }
         [Required]
